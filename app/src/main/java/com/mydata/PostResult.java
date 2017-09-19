cc873be0e9e94ff164052dfafc47c115e73b2d66
ysj <ysjr-2002@163.com>
2017-09-19 19:07:52 +0800

    ok

---
+++ app/src/main/java/com/mydata/PostResult.java
@@ -0,0 +1,14 @@
+package com.mydata;
+
+import java.util.List;
+
+/**
+ * Created by ysj on 2017/9/19.
+ */
+
+public class PostResult {
+
+        public int status;
+        public String info;
+        public List<Object> data;
+}
