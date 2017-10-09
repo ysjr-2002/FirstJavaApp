15ec12d5e8d9db8fec7b737f28637035fa80dbb2
ysj <ysjr-2002@163.com>
2017-10-09 16:22:31 +0800

    ok

--- app/src/main/java/com/myjava/Main3Activity.java
+++ app/src/main/java/com/myjava/Main3Activity.java
@@ -3,15 +3,11 @@ package com.myjava;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.health.TimerStat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.Exchanger;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,6 +19,8 @@ public class Main3Activity extends Activity {
     Button button1;
     @Bind(R.id.textview_info)
     TextView textviewInfo;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,57 +38,73 @@ public class Main3Activity extends Activity {
     @OnClick(R.id.button_1)
     public void onViewClicked(View view) {
 
-        String str = "";
-        StringBuilder sb = new StringBuilder();
-        sb.append("java is shit");
-        textviewInfo.setText(sb);
+        if (progressBar.getVisibility() == View.VISIBLE) {
+            progressBar.setVisibility(View.GONE);
+        } else if (progressBar.getVisibility() == View.GONE) {
+            progressBar.setVisibility(View.VISIBLE);
+        }
 
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                StringBuffer buffer = new StringBuffer();
-                buffer.append("C# is good");
-                textviewInfo.setText(buffer);
-            }
-        }, 5000);
 
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
+//        String str = "";
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("java is shit");
+//        textviewInfo.setText(sb);
+//
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                StringBuffer buffer = new StringBuffer();
+//                buffer.append("C# is good");
+//                textviewInfo.setText(buffer);
+//            }
+//        }, 5000);
+//
+//        TimerTask timerTask = new TimerTask() {
+//            @Override
+//            public void run() {
+//
+//                final StringBuffer sb = new StringBuffer();
+//                sb.append("StringBuffer is ok");
+//
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        textviewInfo.setText(sb);
+//                    }
+//                });
+//            }
+//        };
+//
+//        Timer timer = new Timer();
+//        timer.schedule(timerTask, 10 * 1000);
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(15 * 1000);
+//                } catch (Exception ex) {
+//
+//                }
+//
+//                final  String temp = "thread is ok";
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                        textviewInfo.setText(temp);
+//                    }
+//                });
+//            }
+//        }).start();
+    }
 
-                final StringBuffer sb = new StringBuffer();
-                sb.append("StringBuffer is ok");
+    private void test() {
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        textviewInfo.setText(sb);
-                    }
-                });
-            }
-        };
+        String str = "shit";
+        String temp = str.toString();
 
-        Timer timer = new Timer();
-        timer.schedule(timerTask, 10 * 1000);
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(15 * 1000);
-                } catch (Exception ex) {
-
-                }
-
-                final  String temp = "thread is ok";
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        textviewInfo.setText(temp);
-                    }
-                });
-            }
-        }).start();
+        Integer age = 40;
+        String sex = age.toString();
     }
 }
